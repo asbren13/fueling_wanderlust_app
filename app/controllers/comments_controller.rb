b71977c0491fcc5d160fefc7a65cb53914c866a1
Ashlinn Brennan <asbren13@gmail.com>
2015-02-07 18:46:09 -0500

    all feature tests are now passing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,9 @@
 class CommentsController < ApplicationController
-  before_action :set_city, except: :catalog
-  before_action :set_suggestion, except: :catalog
+  before_action :set_city
+  before_action :set_suggestion
 
   def index
-    @comments = @suggestion.comments.all
-    @comment = Comment.new
-  end
-
-  def show
-    @comment = @suggestion.comments.find(params[:id])
+    @comments = @suggestion.comments
   end
 
   def new
@@ -18,28 +13,26 @@ class CommentsController < ApplicationController
  def create
     @comment = @suggestion.comments.build(comment_params)
     if @comment.save
-      redirect_to city_suggestion_comments_path(@comment, @suggestion, @city), notice: 'Comment was successfully created.'
+      redirect_to city_suggestion_path(@city, @suggestion), notice: 'Comment was successfully created.'
     else
       render :new
     end
   end
 
-
-  def catalog
-    @comments = comment.all
+  def show
+    @comment = @suggestion.comments.find(params[:id])
   end
 
-  private
+private
+  def set_city
+    @city = City.find(params[:city_id])
+  end
 
-    def set_city
-      @city = City.find(params[:city_id])
-    end
+  def set_suggestion
+    @suggestion = @city.suggestions.find(params[:suggestion_id])
+  end
 
-    def set_suggestion
-      @suggestion = @city.suggestions.find(params[:suggestion_id])
-    end
-
-    def comment_params
-      params.require(:comment).permit(:comment)
-    end
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
